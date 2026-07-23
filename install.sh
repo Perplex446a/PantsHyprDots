d567b5ff426eb2077a7e7ae25dba1ea4624649ca
Perplex446 <aaron63b@gmail.com>
2026-07-23 09:29:47 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -1,1 +1,21 @@
-
+echo "Warning! This will replace all of your dotfiles!"
+read -rp "Continue? [y/n]" answer
+echo "replacing hypr directory"
+sleep 1
+rm -rf ~/.config/hypr
+mv ~/hypr ~/.config
+echo "Replacing Waybar directory"
+sleep 1
+rm -rf ~/.config/waybar
+mv ~/waybar ~/.config
+echo "Replacing kitty directory"
+sleep 1
+rm -rf ~/.config/kitty
+mv ~/kitty ~/.config
+echo "Replacing Fuzzel Directory"
+sleep 1
+rm -rf ~/.config/fuzzel
+mv ~/fuzzel ~/.config
+sleep 1
+echo ""
+echo "Done!"
