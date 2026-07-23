9a79d0cf316aa028d67ea44dc9d40dc099d03540
Perplex446 <aaron63b@gmail.com>
2026-07-23 09:42:07 +0100

    Installation script

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@ read -rp "Continue? [y/n]" answer
 echo "replacing hypr directory"
 sleep 1
 rm -rf ~/.config/hypr
-mv ~/PantsHyprDotfiles/hypr ~/.config
+mv ~/PantsHyprDots/hypr ~/.config
 echo "Replacing Waybar directory"
 sleep 1
 rm -rf ~/.config/waybar
-mv ~/PantsHyprDotfiles/waybar ~/.config
+mv ~/PantsHyprDots/waybar ~/.config
 echo "Replacing kitty directory"
 sleep 1
 rm -rf ~/.config/kitty
-mv ~/PantsHyprDotfiles/kitty ~/.config
+mv ~/PantsHyprDots/kitty ~/.config
 echo "Replacing Fuzzel Directory"
 sleep 1
 rm -rf ~/.config/fuzzel
-mv ~/PantsHyprDotfiles/fuzzel ~/.config
+mv ~/PantsHyprDots/fuzzel ~/.config
 sleep 1
 echo ""
 echo "Done!"
